668525d99a2c3c57636bd8ac662cfc4689244cda
Reem Jadallah <reem@freightos.com>
2022-09-01 10:19:49 +0300

    Add blue colors

--- lib/tokens.dart
+++ lib/tokens.dart
@@ -1,4 +1,6 @@
 class Tokens {
-  static const ds_color_accent_purple_5 = 0XFFE7D8FC;
-  static const ds_color_accent_purple_10 = 0XFF9483AF;
+  static const color_accent_purple_5 = 0XFFE7D8FC;
+  static const color_accent_purple_10 = 0XFF9483AF;
+  static const color_light_blue_10 = 0XFFACE8F0;
+  static const color_dark_blue_5 = 0XFF2568A2;
 }
