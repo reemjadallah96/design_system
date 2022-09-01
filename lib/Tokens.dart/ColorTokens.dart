0bbc92844b0c11302adfcefc890f228234eb8bf8
Reem Jadallah <reem@freightos.com>
2022-09-01 14:54:28 +0300

    update readme file with Color pallete and update color names

--- lib/Tokens.dart/ColorTokens.dart
+++ lib/Tokens.dart/ColorTokens.dart
@@ -1,6 +1,18 @@
 class ColorTokens {
-  static const accent_purple_5 = 0XFFE7D8FC;
-  static const accent_purple_10 = 0XFF9483AF;
-  static const light_blue_10 = 0XFFACE8F0;
-  static const dark_blue_5 = 0XFF2568A2;
+  static const accent_purple_10 = 0XFFE7D8FC;
+  static const accent_purple_20 = 0XFF9483AF;
+  static const accent_purple_40 = 0XFF735598;
+  static const primary_blue_5 = 0xFFE1F5F9;
+  static const primary_blue_10 = 0xFFB9EAF5;
+  static const primary_blue_20 = 0XFFACE8F0;
+  static const primary_blue_30 = 0XFF2568A2;
+  static const primary_green_5 = 0XFFAEF4BD;
+  static const deep_green_40 = 0xFF036E4E;
+  static const primary_grey_5 = 0xFFF5F5F5;
+  static const primary_grey_10 = 0xFFE6E7E7;
+  static const primary_grey_20 = 0xFFE5E6E6;
+  static const primary_grey_30 = 0xFF999C9D;
+  static const primary_grey_40 = 0xFF53575F;
+  static const primary_grey_50 = 0xFF54575E;
+  static const primary_grey_60 = 0xFF232537;
 }
