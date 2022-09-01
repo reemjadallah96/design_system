20ef0372c80792e7fc287a41106d28b9eb2f3ebe
Reem Jadallah <reem@freightos.com>
2022-09-01 12:02:40 +0300

    Extract and export ColorTokens from design_system file

---
+++ lib/Tokens.dart/ColorTokens.dart
@@ -0,0 +1,6 @@
+class ColorTokens {
+  static const accent_purple_5 = 0XFFE7D8FC;
+  static const accent_purple_10 = 0XFF9483AF;
+  static const light_blue_10 = 0XFFACE8F0;
+  static const dark_blue_5 = 0XFF2568A2;
+}
