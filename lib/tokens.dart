74d4ff7586e942b045a2c7c0ba53a4e0efd6d28c
Reem Jadallah <reem@freightos.com>
2022-09-01 11:00:42 +0300

    Add changelog.md file and fix tokens file name

--- lib/tokens.dart
+++
@@ -1,6 +0,0 @@
-class Tokens {
-  static const color_accent_purple_5 = 0XFFE7D8FC;
-  static const color_accent_purple_10 = 0XFF9483AF;
-  static const color_light_blue_10 = 0XFFACE8F0;
-  static const color_dark_blue_5 = 0XFF2568A2;
-}
